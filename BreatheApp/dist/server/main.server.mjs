febcd20ffb34267517b8dd9fcafec9588dfd5d9a
Ghilas Amarouche <6194975@claurendeau.qc.ca>
2025-02-27 22:11:52 -0500

    preparing for netifly  deploying

---
+++ BreatheApp/dist/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-5PNNKAN6.mjs";import"./chunk-NBP6Z6GY.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- BreatheApp/src/environments.ts
+++ BreatheApp/src/environments.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    url:process.env['NG_APP_BASE_URL'],
     production: false,
     stockApiKey: 'd8e0d845fcb7b0b91844ffd5a5addba3'
 };
